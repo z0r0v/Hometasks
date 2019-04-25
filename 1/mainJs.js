c86b0aa65416e08d6b208d03ac78ec9652b73545
Alex Zhorov <3767082@mail.ru>
2019-04-25 11:42:22 +0300

    Косметика

--- 1/mainJs.js
+++ 1/mainJs.js
@@ -3,20 +3,22 @@ function MyFunctionPlus() {
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) + parseInt(textValue2);
     ResCol.innerHTML = result;
-
 }
+
 function MyFunctionSubtraction() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) - parseInt(textValue2);
     ResCol.innerHTML = result;
 }
+
 function MyFunctionDivision() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) / parseInt(textValue2);
     ResCol.innerHTML = result;
 }
+
 function MyFunctionMultiply() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
