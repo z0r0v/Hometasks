9b3286236e30ca5eb2238527cb9e1be578b14925
Alex Zhorov <3767082@mail.ru>
2019-04-25 16:19:51 +0300

    более читаемый код

--- 1/mainJs.js
+++ 1/mainJs.js
@@ -25,31 +25,40 @@ function MyFunctionMultiply() {
     let result = parseInt(textValue1) * parseInt(textValue2);
     ResCol.innerHTML = result;
 }
+
 /*Кнопки для первой графы*/
 function MyFunction1() {
     document.getElementById('incomingDataFirstField').value = 1
 }
+
 function MyFunction2() {
     document.getElementById('incomingDataFirstField').value = 2
 }
+
 function MyFunction3() {
     document.getElementById('incomingDataFirstField').value = 3
 }
+
 function MyFunction4() {
     document.getElementById('incomingDataFirstField').value = 4
 }
+
 function MyFunction5() {
     document.getElementById('incomingDataFirstField').value = 5
 }
+
 function MyFunction6() {
     document.getElementById('incomingDataFirstField').value = 6
 }
+
 function MyFunction7() {
     document.getElementById('incomingDataFirstField').value = 7
 }
+
 function MyFunction8() {
     document.getElementById('incomingDataFirstField').value = 8
 }
+
 function MyFunction9() {
     document.getElementById('incomingDataFirstField').value = 9
 }
