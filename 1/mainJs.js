77d7abdf105b3f5d03199b42a60639e469812c54
Alex Zhorov <3767082@mail.ru>
2019-04-25 20:37:45 +0300

    изменена функция теперь в главном инпуте выводятся значения

--- 1/mainJs.js
+++ 1/mainJs.js
@@ -2,63 +2,63 @@ function MyFunctionPlus() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) + parseInt(textValue2);
-    ResCol.innerHTML = result;
+    incomingDataFirstField.value =result;
 }
 
 function MyFunctionSubtraction() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) - parseInt(textValue2);
-    ResCol.innerHTML = result;
+    incomingDataFirstField.value =result;
 }
 
 function MyFunctionDivision() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) / parseInt(textValue2);
-    ResCol.innerHTML = result.toFixed(2);
+    incomingDataFirstField.value = result.toFixed(2);
 }
 
 function MyFunctionMultiply() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) * parseInt(textValue2);
-    ResCol.innerHTML = result;
+    incomingDataFirstField.value = result;
 }
 
 /*Кнопки для первой графы*/
 function MyFunction1() {
-    document.getElementById('incomingDataFirstField').value = 1
+   incomingDataFirstField.value += "1";
 }
 
 function MyFunction2() {
-    document.getElementById('incomingDataFirstField').value = 2
+    incomingDataFirstField.value += "2";
 }
 
 function MyFunction3() {
-    document.getElementById('incomingDataFirstField').value = 3
+    incomingDataFirstField.value += "3";
 }
 
 function MyFunction4() {
-    document.getElementById('incomingDataFirstField').value = 4
+    incomingDataFirstField.value += "4";
 }
 
 function MyFunction5() {
-    document.getElementById('incomingDataFirstField').value = 5
+    incomingDataFirstField.value += "5";
 }
 
 function MyFunction6() {
-    document.getElementById('incomingDataFirstField').value = 6
+    incomingDataFirstField.value += "6";
 }
 
 function MyFunction7() {
-    document.getElementById('incomingDataFirstField').value = 7
+    incomingDataFirstField.value += "7";
 }
 
 function MyFunction8() {
-    document.getElementById('incomingDataFirstField').value = 8
+    incomingDataFirstField.value += "8";
 }
 
 function MyFunction9() {
-    document.getElementById('incomingDataFirstField').value = 9
+    incomingDataFirstField.value += "9";
 }
