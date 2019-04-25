ee815b195dee8d628b9a1127f5e559ef81758a49
Alex Zhorov <3767082@mail.ru>
2019-04-25 21:35:36 +0300

    Функция валидации

--- 1/mainJs.js
+++ 1/mainJs.js
@@ -1,64 +1,71 @@
+/*function valueInp() {
+    if (typeof(document.getElementById('incomingDataFirstField').value) === "number") {
+    } else
+    alert('Введите число правильно');*/
+/*Это валидация но пока что фигня какаято*/
+
+
 function MyFunctionPlus() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) + parseInt(textValue2);
-    incomingDataFirstField.value =result;
+    document.getElementById('incomingDataFirstField').value = result;
 }
 
 function MyFunctionSubtraction() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) - parseInt(textValue2);
-    incomingDataFirstField.value =result;
+    document.getElementById('incomingDataFirstField').value = result;
 }
 
 function MyFunctionDivision() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) / parseInt(textValue2);
-    incomingDataFirstField.value = result.toFixed(2);
+    document.getElementById('incomingDataFirstField').value = result.toFixed(2);
 }
 
 function MyFunctionMultiply() {
     let textValue1 = document.getElementById('incomingDataFirstField').value;
     let textValue2 = document.getElementById('incomingDataSecondField').value;
     let result = parseInt(textValue1) * parseInt(textValue2);
-    incomingDataFirstField.value = result;
+    document.getElementById('incomingDataFirstField').value = result;
 }
 
 /*Кнопки для первой графы*/
 function MyFunction1() {
-   incomingDataFirstField.value += "1";
+    document.getElementById('incomingDataFirstField').value += "1";
 }
 
 function MyFunction2() {
-    incomingDataFirstField.value += "2";
+    document.getElementById('incomingDataFirstField').value += "2";
 }
 
 function MyFunction3() {
-    incomingDataFirstField.value += "3";
+    document.getElementById('incomingDataFirstField').value += "3";
 }
 
 function MyFunction4() {
-    incomingDataFirstField.value += "4";
+    document.getElementById('incomingDataFirstField').value += "4";
 }
 
 function MyFunction5() {
-    incomingDataFirstField.value += "5";
+    document.getElementById('incomingDataFirstField').value += "5";
 }
 
 function MyFunction6() {
-    incomingDataFirstField.value += "6";
+    document.getElementById('incomingDataFirstField').value += "6";
 }
 
 function MyFunction7() {
-    incomingDataFirstField.value += "7";
+    document.getElementById('incomingDataFirstField').value += "7";
 }
 
 function MyFunction8() {
-    incomingDataFirstField.value += "8";
+    document.getElementById('incomingDataFirstField').value += "8";
 }
 
 function MyFunction9() {
-    incomingDataFirstField.value += "9";
+    document.getElementById('incomingDataFirstField').value += "9";
 }
