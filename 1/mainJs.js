5641094d53ebc71bee21b2552951d36e40f0e6c6
Alex Zhorov <3767082@mail.ru>
2019-04-25 09:03:43 +0300

    ЗАдание готово

---
+++ 1/mainJs.js
@@ -0,0 +1,25 @@
+function MyFunctionPlus() {
+    let textValue1 = document.getElementById('incomingDataFirstField').value;
+    let textValue2 = document.getElementById('incomingDataSecondField').value;
+    let result = parseInt(textValue1) + parseInt(textValue2);
+    ResCol.innerHTML = result;
+
+}
+function MyFunctionSubtraction() {
+    let textValue1 = document.getElementById('incomingDataFirstField').value;
+    let textValue2 = document.getElementById('incomingDataSecondField').value;
+    let result = parseInt(textValue1) - parseInt(textValue2);
+    ResCol.innerHTML = result;
+}
+function MyFunctionDivision() {
+    let textValue1 = document.getElementById('incomingDataFirstField').value;
+    let textValue2 = document.getElementById('incomingDataSecondField').value;
+    let result = parseInt(textValue1) / parseInt(textValue2);
+    ResCol.innerHTML = result;
+}
+function MyFunctionMultiply() {
+    let textValue1 = document.getElementById('incomingDataFirstField').value;
+    let textValue2 = document.getElementById('incomingDataSecondField').value;
+    let result = parseInt(textValue1) * parseInt(textValue2);
+    ResCol.innerHTML = result;
+}
