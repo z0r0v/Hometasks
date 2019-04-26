3f337b3e3082c74b241556010290ff891f943209
Alex Zhorov <3767082@mail.ru>
2019-04-26 09:32:11 +0300

    Сокращена функция кнопок

--- 1/mainJs.js
+++ 1/mainJs.js
@@ -4,10 +4,9 @@
     alert('Введите число правильно');*/
 /*Это валидация но пока что фигня какаято*/
 
-
 function MyFunctionPlus() {
-    let textValue1 = document.getElementById('incomingDataFirstField').value;
-    let textValue2 = document.getElementById('incomingDataSecondField').value;
+    let textValue1 = document.getElementById('incomingDataFirstField');
+    let textValue2 = document.getElementById('incomingDataSecondField');
     let result = parseInt(textValue1) + parseInt(textValue2);
     document.getElementById('incomingDataFirstField').value = result;
 }
@@ -34,38 +33,6 @@ function MyFunctionMultiply() {
 }
 
 /*Кнопки для первой графы*/
-function MyFunction1() {
-    document.getElementById('incomingDataFirstField').value += "1";
-}
-
-function MyFunction2() {
-    document.getElementById('incomingDataFirstField').value += "2";
-}
-
-function MyFunction3() {
-    document.getElementById('incomingDataFirstField').value += "3";
-}
-
-function MyFunction4() {
-    document.getElementById('incomingDataFirstField').value += "4";
-}
-
-function MyFunction5() {
-    document.getElementById('incomingDataFirstField').value += "5";
-}
-
-function MyFunction6() {
-    document.getElementById('incomingDataFirstField').value += "6";
-}
-
-function MyFunction7() {
-    document.getElementById('incomingDataFirstField').value += "7";
-}
-
-function MyFunction8() {
-    document.getElementById('incomingDataFirstField').value += "8";
-}
-
-function MyFunction9() {
-    document.getElementById('incomingDataFirstField').value += "9";
+function MyFunction(value) {
+    document.getElementById('incomingDataFirstField').value += value;
 }
