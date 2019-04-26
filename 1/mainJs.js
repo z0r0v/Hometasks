3287e2a02466605c635de540e3c9958d407226c8
Alex Zhorov <3767082@mail.ru>
2019-04-26 09:42:10 +0300

    Сокращена функция кнопок

--- 1/mainJs.js
+++ 1/mainJs.js
@@ -1,38 +1,53 @@
-/*function valueInp() {
-    if (typeof(document.getElementById('incomingDataFirstField').value) === "number") {
-    } else
-    alert('Введите число правильно');*/
-/*Это валидация но пока что фигня какаято*/
+/*Валидация*/
+function valueInp() {
+    let isValid;
+    if (!isNaN(parseInt(document.getElementById('incomingDataFirstField').value))) {
+        isValid = true;
+    } else {
+        alert('Введите число правильно');
+        isValid = false;
+    }
+    return isValid;
+}
 
+/*Функции кнопок приводящихк результатам " + - * / " */
 function MyFunctionPlus() {
-    let textValue1 = document.getElementById('incomingDataFirstField');
-    let textValue2 = document.getElementById('incomingDataSecondField');
-    let result = parseInt(textValue1) + parseInt(textValue2);
-    document.getElementById('incomingDataFirstField').value = result;
+    if (valueInp()) {
+        let textValue1 = document.getElementById('incomingDataFirstField').value;
+        let textValue2 = document.getElementById('incomingDataSecondField').value;
+        let result = parseInt(textValue1) + parseInt(textValue2);
+        document.getElementById('incomingDataFirstField').value = result;
+    }
 }
 
 function MyFunctionSubtraction() {
-    let textValue1 = document.getElementById('incomingDataFirstField').value;
-    let textValue2 = document.getElementById('incomingDataSecondField').value;
-    let result = parseInt(textValue1) - parseInt(textValue2);
-    document.getElementById('incomingDataFirstField').value = result;
+    if (valueInp()) {
+        let textValue1 = document.getElementById('incomingDataFirstField').value;
+        let textValue2 = document.getElementById('incomingDataSecondField').value;
+        let result = parseInt(textValue1) - parseInt(textValue2);
+        document.getElementById('incomingDataFirstField').value = result;
+    }
 }
 
 function MyFunctionDivision() {
-    let textValue1 = document.getElementById('incomingDataFirstField').value;
-    let textValue2 = document.getElementById('incomingDataSecondField').value;
-    let result = parseInt(textValue1) / parseInt(textValue2);
-    document.getElementById('incomingDataFirstField').value = result.toFixed(2);
+    if (valueInp()) {
+        let textValue1 = document.getElementById('incomingDataFirstField').value;
+        let textValue2 = document.getElementById('incomingDataSecondField').value;
+        let result = parseInt(textValue1) / parseInt(textValue2);
+        document.getElementById('incomingDataFirstField').value = result.toFixed(2);
+    }
 }
 
 function MyFunctionMultiply() {
-    let textValue1 = document.getElementById('incomingDataFirstField').value;
-    let textValue2 = document.getElementById('incomingDataSecondField').value;
-    let result = parseInt(textValue1) * parseInt(textValue2);
-    document.getElementById('incomingDataFirstField').value = result;
+    if (valueInp()) {
+        let textValue1 = document.getElementById('incomingDataFirstField').value;
+        let textValue2 = document.getElementById('incomingDataSecondField').value;
+        let result = parseInt(textValue1) * parseInt(textValue2);
+        document.getElementById('incomingDataFirstField').value = result;
+    }
 }
 
-/*Кнопки для первой графы*/
+/*Кнопки для первой графы собранны по примеру в стак*/
 function MyFunction(value) {
     document.getElementById('incomingDataFirstField').value += value;
 }
