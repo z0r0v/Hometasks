14ea6294a08441f938cf0b8433221c04f9ea0d90
Alex Zhorov <3767082@mail.ru>
2019-04-29 10:55:31 +0300

    Completed task 4

--- bodyBagraund/mainJs.js
+++ bodyBagraund/mainJs.js
@@ -11,7 +11,15 @@ function listSelect(){
 }
 /*here I change the class for the body element and the classes are written in CSS*/
 function myListClass(){
-    document.body.style.backgroundColor=null;
+    document.body.style.backgroundColor=null;/*reset style value*/
     let myListValueClass = document.querySelector('.myListClass');
     document.body.className = myListValueClass.value;
+}
+
+/*here I change the class for the body element in CSS*/
+function applyColor(){
+    document.body.style.backgroundColor=null;/*reset style value*/
+    document.body.className=null;/*reset class value*/
+    let myListValueClass = document.querySelector('.inColorCss');
+    document.body.style.setProperty('--main-color', myListValueClass.value);
 }
