30994ff4a9168d2d0f3539414d3837237af0d337
Alex Zhorov <3767082@mail.ru>
2019-04-30 11:45:44 +0300

    Optimization code

--- bodyBagraund/mainJs.js
+++ bodyBagraund/mainJs.js
@@ -1,36 +1,43 @@
-/*here I change the style through slim body in the document get into classes*/
+/*Обьявляем переменнуые в функции для изменения значения к нулю */
+function bgStlNul() {
+    /*To shorten the code, it took variables out of functions*/
+    let style = document.body.style;
+    let clanNameNul = document.body;
+    /*Приравниваю занчения цвета и класса к nul*/
+    style.backgroundColor = null;/*reset style value*/
+    clanNameNul.className = null;/*reset class value*/
+}
+
+/*№1 here I change the style through slim body in the document get into classes*/
 function cheColor() {
+    bgStlNul();
     let newColor = document.querySelector(".inColor");
     document.body.style.backgroundColor = newColor.value;
 }
 
-/*here I change the style from the drop-down list, I get into classes*/
+/*№2 here I change the style from the drop-down list, I get into classes*/
 function listSelect(){
+    bgStlNul();
     let myListValue = document.querySelector('.myList');
     document.body.style.backgroundColor = myListValue.value;
 }
 
-/*To shorten the code, it took variables out of functions*/
-let bgNul = document.body.style.backgroundColor;
-let clanNameNul = document.body.className;
-
-/*here I change the class for the body element and the classes are written in CSS*/
+/*№3 here I change the class for the body element and the classes are written in CSS*/
 function myListClass(){
-    bgNul = null;/*reset style value*/
+    bgStlNul();
     let myListValueClass = document.querySelector('.myListClass');
     document.body.className = myListValueClass.value;
 }
 
-/*here I change the class for the body element in CSS*/
+/*№4 here I change the class for the body element in CSS*/
 function applyColor(){
-    bgNul = null;/*reset style value*/
-    clanNameNul = null;/*reset class value*/
+    bgStlNul();
     let myListValueClass = document.querySelector('.inColorCss');
     document.body.style.setProperty('--main-color', myListValueClass.value);
 }
 
+/*№5 here I change the class for the body element in CSS no Button*/
 function myListChangeThis(selectElement){
-    bgNul = null;/*reset style value*/
-    clanNameNul = null;/*reset class value*/
+    bgStlNul();
     document.body.style.setProperty('--main-color', selectElement.value);
 }
