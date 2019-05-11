b6c4792f6e03ec2222d7a73149e7d0465efd1f13
Alex Zhorov <3767082@mail.ru>
2019-05-11 17:28:44 +0300

    vers 1.1

--- №3 questioning/dz Plus/main.js
+++ №3 questioning/dz Plus/main.js
@@ -6,7 +6,8 @@ function initHTMLElements() {
   htmlElements.span = document.querySelector('span');
   htmlElements.body = document.body;
   htmlElements.img = document.querySelector('img');
-    htmlElements.p = document.querySelector('p');
+  htmlElements.imgJs = document.querySelector('img.js');
+  htmlElements.p = document.querySelector('p');
 }
 
 function initEvenHandles() {
@@ -32,38 +33,44 @@ let currentValue;
 
 function matFunction() {
     currentValue = getUniqueRandomValue(myArray, currentValue);
-    console.log(currentValue);
-    htmlElements.body.className = currentValue;
+
+    /*color from className*/
+    /*htmlElements.body.className = currentValue;*/
+
+    /*color from var(--color)*/
+    htmlElements.body.style.setProperty('--color', currentValue);
 
     function textColorSnapColorCheimg() {
         htmlElements.span.style.color = "white";
         htmlElements.img.src = "http://svdsecurity.by/alexFiles/logo/logoZhorovWhite.png";
         htmlElements.p.style.color = "white";
+        /*добавляю цвет в боди*/
     }
+    textColorSnapColorCheimg();
 
-    if(htmlElements.body.className ==="Pink"){
-        htmlElements.span.innerHTML = "Java Script has a development trend!";
-        textColorSnapColorCheimg();
-
-    }else if(htmlElements.body.className ==="LightPink"){
-        htmlElements.span.innerHTML = "JavaScript сompiles and interpreted in many programming languages!";
-        textColorSnapColorCheimg();
+    switch (currentValue) {
+        case "Pink":
+            htmlElements.span.innerHTML = "Java Script has a development trend!";
+            htmlElements.imgJs.src ="http://svdsecurity.by/alexFiles/img/js.png";
+            break;
+        case "LightPink":
+            htmlElements.span.innerHTML = "JavaScript сompiles and interpreted in many programming languages!";
+            htmlElements.imgJs.src ="https://www.moltin.com/hubfs/js-cube.svg";
+            break;
+        case "HotPink":
+            htmlElements.span.innerHTML = "Available on all devices and browsers!";
+            htmlElements.imgJs.src ="http://svdsecurity.by/alexFiles/img/js.png";
+            break;
+        case "DeepPink":
+            htmlElements.span.innerHTML = "Has the ability to immediately see the result and manage multimedia space!";
+            htmlElements.imgJs.src ="https://www.moltin.com/hubfs/js-cube.svg";
+            break;
+        case "MediumVioletRed":
+            htmlElements.span.innerHTML = "The demand for javascript programmers increases by 30% per year!";
+            break;
+        case "PaleVioletRed":
+            htmlElements.span.innerHTML = "On tut.by: experts of foreign economic activity 3 pages, and on a JavaScript programmer 15 pages!";
+            htmlElements.imgJs.src ="https://www.moltin.com/hubfs/js-cube.svg";
+            break;
     }
-    else if(htmlElements.body.className ==="HotPink"){
-        htmlElements.span.innerHTML = "Available on all devices and browsers!";
-        textColorSnapColorCheimg();
-    }
-    else if(htmlElements.body.className ==="DeepPink"){
-        htmlElements.span.innerHTML = "Has the ability to immediately see the result and manage multimedia space!";
-        textColorSnapColorCheimg();
-    }
-    else if(htmlElements.body.className ==="MediumVioletRed"){
-        htmlElements.span.innerHTML = "The demand for javascript programmers increases by 30% per year!";
-        textColorSnapColorCheimg();
-    }
-    else if(htmlElements.body.className ==="PaleVioletRed"){
-        htmlElements.span.innerHTML = "On tut.by: experts of foreign economic activity 3 pages, and on a JavaScript programmer 15 pages!";
-        textColorSnapColorCheimg();
-    }
-
 }
