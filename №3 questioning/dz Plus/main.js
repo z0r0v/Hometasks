5f79eeab10166adf68b05af8eae37fe2d3bf56e0
Alex Zhorov <3767082@mail.ru>
2019-05-11 16:01:10 +0300

    Реорганизован принцип изменения картинок

--- №3 questioning/dz Plus/main.js
+++ №3 questioning/dz Plus/main.js
@@ -16,63 +16,54 @@ function initEvenHandles() {
 initHTMLElements();
 initEvenHandles();
 
+/*Функция выдающая рандомные значения для value*/
+let myArray = ["Pink", "LightPink", "HotPink", "DeepPink","MediumVioletRed", "PaleVioletRed"];
+function getRandomValue(arr, max) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+function getUniqueRandomValue(arr, currentValue) {
+    let randomNumber;
+    do {
+        randomNumber = getRandomValue(arr, arr.length);
+    } while (randomNumber === currentValue);
+    return randomNumber;
+}
+let currentValue;
 
 function matFunction() {
+    currentValue = getUniqueRandomValue(myArray, currentValue);
+    console.log(currentValue);
+    htmlElements.body.className = currentValue;
 
-
-    let exists = [],value, randomNumber, max = 5;
-    for(let l = 0; l < max; l++) {
-        do {
-        randomNumber = Math.floor(Math.random() * max);
-        } while (exists[randomNumber]);
-        exists[randomNumber] = true;
-        value = randomNumber;
-}
-
-    switch (value) {
-        case 0:
-            htmlElements.span.innerHTML = "Java Script has a development trend!";
-            htmlElements.span.style.color ="white";
-            htmlElements.body.className = "Pink";
-            htmlElements.img.src = "img/logoZhorovWhite.png";
-            htmlElements.p.style.color ="white";
-
-
-            break;
-        case 1:
-            htmlElements.span.innerHTML = "JavaScript сompiles and interpreted in many programming languages!";
-            htmlElements.span.style.color ="white";
-            htmlElements.body.className = "LightPink";
-            htmlElements.img.src = "img/logoZhorovWhite.png";
-            htmlElements.p.style.color ="white";
-            break;
-        case 2:
-            htmlElements.span.innerHTML = "Available on all devices and browsers!";
-            htmlElements.span.style.color ="white";
-            htmlElements.body.className = "HotPink";
-            htmlElements.img.src = "img/logoZhorovWhite.png";
-            htmlElements.p.style.color ="white";
-            break;
-        case 3:
-            htmlElements.span.innerHTML = "Has the ability to immediately see the result and manage multimedia space!";
-            htmlElements.span.style.color ="white";
-            htmlElements.body.className = "DeepPink";
-            htmlElements.img.src = "img/logoZhorovWhite.png";
-            htmlElements.p.style.color ="white";
-            break;
-        case 4:
-            htmlElements.span.innerHTML = "The demand for javascript programmers increases by 30% per year!";
-            htmlElements.span.style.color ="white";
-            htmlElements.body.className = "MediumVioletRed";
-            htmlElements.img.src = "img/logoZhorovWhite.png";
-            htmlElements.p.style.color ="white";
-            break;
-        case 5:
-            htmlElements.span.innerHTML = "On tut.by: experts of foreign economic activity 3 pages, and on a JavaScript programmer 15 pages!";
-            htmlElements.span.style.color ="white";
-            htmlElements.body.className = "PaleVioletRed";
-            htmlElements.img.src = "img/logoZhorovWhite.png";
-            htmlElements.p.style.color ="white";
-            break;
+    function textColorSnapColorCheimg() {
+        htmlElements.span.style.color = "white";
+        htmlElements.img.src = "img/logoZhorovWhite.png";
+        htmlElements.p.style.color = "white";
     }
+
+    if(htmlElements.body.className ==="Pink"){
+        htmlElements.span.innerHTML = "Java Script has a development trend!";
+        textColorSnapColorCheimg();
+
+    }else if(htmlElements.body.className ==="LightPink"){
+        htmlElements.span.innerHTML = "JavaScript сompiles and interpreted in many programming languages!";
+        textColorSnapColorCheimg();
+    }
+    else if(htmlElements.body.className ==="HotPink"){
+        htmlElements.span.innerHTML = "Available on all devices and browsers!";
+        textColorSnapColorCheimg();
+    }
+    else if(htmlElements.body.className ==="DeepPink"){
+        htmlElements.span.innerHTML = "Has the ability to immediately see the result and manage multimedia space!";
+        textColorSnapColorCheimg();
+    }
+    else if(htmlElements.body.className ==="MediumVioletRed"){
+        htmlElements.span.innerHTML = "The demand for javascript programmers increases by 30% per year!";
+        textColorSnapColorCheimg();
+    }
+    else if(htmlElements.body.className ==="PaleVioletRed"){
+        htmlElements.span.innerHTML = "On tut.by: experts of foreign economic activity 3 pages, and on a JavaScript programmer 15 pages!";
+        textColorSnapColorCheimg();
+    }
+
 }
