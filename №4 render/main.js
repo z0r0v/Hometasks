54287d6d7c83af8c0a40fa880e3e541fbebb5cd6
Alex Zhorov <3767082@mail.ru>
2019-05-12 23:19:35 +0300

    dz 4 p.2

--- №4 render/main.js
+++ №4 render/main.js
@@ -26,20 +26,43 @@ init();
 
 function render() {
 
-/*<div class="todos">
-        <div class="todo" style="background-color: blue;">
-          <span class="todo-text" style="color: white;">white</span>
-          <button class="delete-button">X</button>
-         </div>
-        <div class="todo" style="background-color: yellow;">
-          <span class="todo-text" style="color: black;">black</span>
-          <button class="delete-button">X</button>
-        </div>
-        <div class="todo" style="background-color: whitesmoke;">
-          <span class="todo-text" style="color: green;">green</span>
-          <button class="delete-button">X</button>
-        </div>
- </div>*/
+/*I know how to make a designer and I did it in DivColor*/
+    function DivColor (color, bgColor){
+    let newDeleteButton = document.createElement('button');
+    newDeleteButton.classList.add("delete-button");
+    newDeleteButton.color = color;
+    newDeleteButton.innerText = "X";
 
+    let newTodoText = document.createElement('span');
+    newTodoText.classList.add("todo-text");
+    newTodoText.innerText = color;
+    newTodoText.style.color = color;
 
+    let newTodo = document.createElement('div');
+    newTodo.classList.add("todo");
+    newTodo.style.backgroundColor = bgColor;
+
+    newTodo.appendChild(newTodoText);
+    newTodo.appendChild(newDeleteButton);
+    htmlElements.todos.appendChild(newTodo);
+    }
+
+    DivColor(todos[0].color,todos[0].bgColor);
+    new DivColor(todos[1].color,todos[1].bgColor);
+    new DivColor(todos[2].color,todos[2].bgColor);
 }
+
+/*Это ждет своего звездного часа*/
+/*function a(){
+    for (let i = 0; i < todos.length; i++) {
+        console.log(todos[i].color);
+    }
+}*/
+
+
+
+
+
+
+
+
