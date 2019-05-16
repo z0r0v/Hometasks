f1b999a3b002312d41426d39a542e6d46877709a
Alex Zhorov <3767082@mail.ru>
2019-05-16 13:23:32 +0300

    Кроме создания масива и кроме масива итого при выб

--- №4 render/main.js
+++ №4 render/main.js
@@ -1,6 +1,8 @@
 let htmlElements;
-let todos = [
-  { color: 'white', bgColor: 'blue' },
+let todos = [{
+    color: 'white',
+    bgColor: 'blue'
+  },
   {
     color: 'black',
     bgColor: 'yellow'
@@ -18,6 +20,7 @@ function init() {
     textColorInput: document.querySelector('input.text-color-input'),
     addButton: document.querySelector('button.add-button'),
     todos: document.querySelector('div.todos'),
+    todo: document.querySelector('div.todo'),
     heloWorldText: document.querySelector('div.hello-world'),
     body: document.body,
   };
@@ -27,83 +30,72 @@ function init() {
 
 init();
 
-function divColor (color, bgColor){
-    let newDeleteButton = document.createElement('button');
-    newDeleteButton.classList.add("delete-button");
-    newDeleteButton.color = color;
-    newDeleteButton.innerText = "X";
-    newDeleteButton.addEventListener('click', deletteButtonCkicked);
+function divColor(color, bgColor) {
+  let newDeleteButton = document.createElement('button');
+  newDeleteButton.classList.add("delete-button");
+  newDeleteButton.color = color;
+  newDeleteButton.innerText = "X";
+  newDeleteButton.addEventListener('click', deletteButtonCkicked);
 
 
-    let newTodoText = document.createElement('span');
-    newTodoText.classList.add("todo-text");
-    newTodoText.innerText = color;
-    newTodoText.style.color = color;
+  let newTodoText = document.createElement('span');
+  newTodoText.classList.add("todo-text");
+  newTodoText.innerText = color;
+  newTodoText.style.color = color;
 
-    let newTodo = document.createElement('div');
-    newTodo.classList.add("todo");
-    newTodo.style.backgroundColor = bgColor;
-    newTodo.addEventListener('click', addBorderColorBg);
+  let newTodo = document.createElement('div');
+  newTodo.classList.add("todo");
+  newTodo.style.backgroundColor = bgColor;
+  newTodo.addEventListener('click', addBorderColorBg);
 
-    newTodo.appendChild(newTodoText);
-    newTodo.appendChild(newDeleteButton);
-    htmlElements.todos.appendChild(newTodo);
+  newTodo.appendChild(newTodoText);
+  newTodo.appendChild(newDeleteButton);
+  htmlElements.todos.appendChild(newTodo);
 }
 
 function render() {
-    for (let i = 0; i < todos.length; i++) {
-        divColor(todos[i].color, todos[i].bgColor);
-    }
+  for (let i = 0; i < todos.length; i++) {
+    divColor(todos[i].color, todos[i].bgColor);
+  }
 }
 
-
-console.log(htmlElements.heloWorldText.style);
 /* Добавляет бордер к выбранному елементу и красит баграунд в цвет */
 function addBorderColorBg() {
   reset();
   // Тут подбор индекса масива будет
   htmlElements.body.style.backgroundColor = todos[0].bgColor;
-  htmlElements.heloWorldText.style.color = "red";
-  todos[0].select = true;/* Временно нулевой елемент */
+  htmlElements.heloWorldText.style.color = todos[0].Color;
+  todos[0].select = true; /* Временно нулевой елемент */
 
-
-/* Функция которая если тру выдает бордер */
-function border(color){
-  if(todos[0].select === true){
-    htmlElements.newTodo.style.border = color, "1px solid";
-  }
-  }
-  border("red");
+  /* Функция которая если тру выдает бордер */
+    if (todos[0].select === true) {/* Врмеменно нулевой елемент */
+      document.querySelector('div.todo').classList.add('selected');
+    }
 };
 
-
-
-
-
 /* функция сброса */
-function reset(){
+function reset() {
   htmlElements.bgColorInput.value = "";
   htmlElements.textColorInput.value = "";
-
-
+  document.querySelector('div.todo').classList.remove('selected');
 }
 
 /* Функция выбора елемента созданного масива соответствующего нашему */
-function checElemenMasev(){
-// суда хочу вынести поиск елемнта масива
-let item = this.parentElement;
-const itemElements = item.parentElement;
-let nodeArray = itemElements.querySelector('div.item');
-const itemArray = Array.from(nodeArray);
-let index = itemArray.indexOf(item);
-item.splice(index, 1);
+function checElemenMasev() {
+  // суда хочу вынести поиск елемнта масива
+  let item = this.parentElement;
+  const itemElements = item.parentElement;
+  let nodeArray = itemElements.querySelector('div.item');
+  const itemArray = Array.from(nodeArray);
+  let index = itemArray.indexOf(item);
+  item.splice(index, 1);
 }
 
 // Вешаем на кнопку делит РАБОТАЕТ НЕ ПРАВИЛЬНО
 function deletteButtonCkicked(event) {
-event.stopPropagation();
-checElemenMasev();/* Вызываю выбор елемента масива */
-render();/* Отрисовываю обьек */
+  event.stopPropagation();
+  checElemenMasev(); /* Вызываю выбор елемента масива */
+  render(); /* Отрисовываю обьек */
 }
 
 
@@ -123,7 +115,7 @@ render();/* Отрисовываю обьек */
 При этом фон и цвет текста заголовка принимают значения цвета текста и фона выбранного 
 div class=”item”
 
-Часть 3
+Часть 3 + -"Это задание"
 Подготовить портфолио в codepen с нумерацией, соответствующей нумерации заданий ДЗ
 Дедлайн
 17 мая 18:00
