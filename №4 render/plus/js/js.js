dfbf733c921920fb6c1104c6a814089a5ad985b5
Alex Zhorov <3767082@mail.ru>
2019-05-18 12:28:06 +0300

    123

--- №4 render/plus/js/js.js
+++ №4 render/plus/js/js.js
@@ -1,49 +1,21 @@
-let reternArray = [
-  {},
+document.onclick = function(e) { // выводит текущие координаты при клике
+  document.getElementById('coords').innerHTML = e.clientX + ':' + e.clientY;
+};
+let field = document.getElementById('field');
 
-]
-    function getMouseCoords(e) {
-      var e = e || window.event;
-      document.getElementsByClassName('div.footballField').innerHTML = e.clientX + ', ' + 
-               e.clientY + '<br>' + e.screenX + ', ' + e.screenY;
-    }
-    
-    let followCursor = (function() {
+let ballArray = []
 
-      let ball = document.createElement('div');
-      ball.style.position = 'absolute';
-      ball.style.margin = '-10px';
-      ball.style.padding = '5px';
-      ball.style.border = '1px solid red';
 
-      let balImg = document.createElement('circle');
-      balImg.id = 'Oval-1';
-      balImg.fill = '#FFFFFF';
-      balImg.cx = '20';
-      balImg.ry = '20';
-      balImg.r = '20';
-      console.log(balImg);
-      
+  field.onclick = function(e){
+  document.querySelector('svg.svg').style.left = e.pageX + "px";
+  document.querySelector('svg.svg').style.top = e.pageY + "px";
 
-    
-      return {
-        init: function() {
-          document.body.appendChild(ball);
-          ball.appendChild(balImg);
-        },
-    
-        run: function(e) {
-          var e = e || window.event;
-          ball.style.left  = (e.clientX - 5) + 'px';
-          ball.style.top = (e.clientY - 5) + 'px';
-          getMouseCoords(e);
-        }
-      };
-    }());
-    
-    window.onload = function() {
-      followCursor.init();
-      document.body.onmousemove = followCursor.run;
+  ballArray.x = this.e.pageX + "px";
+  ballArray.y = this.e.pageY + "px";  
+}
 
-    }
+
+
+console.log(ballArray);
+
 
