47c1f8ae826dec0ce8481b98ab1d6a2b57f1dc1a
Alex Zhorov <3767082@mail.ru>
2019-05-21 14:37:07 +0300

    123

--- №4.1.2.3 №5.1.2.3 №6.1.2 render/plus/js/js.js
+++ №4.1.2.3 №5.1.2.3 №6.1.2 render/plus/js/js.js
@@ -1,23 +1,31 @@
-let ballArray = [{left:'35px', top:'36px'}];
+let ballArray = [{
+  left: '35px',
+  top: '36px'
+}];
 let field = document.getElementById('field');
 let ball = document.querySelector('svg.svg');
 let repeat = document.querySelector('a.myButton');
 repeat.addEventListener('click', goRepeat);
 
-  field.onclick = function(e){
-    let left = ball.style.left = e.pageX + 'px';
-    let top = ball.style.top = e.pageY + 'px';
-    ballArray.push({left, top});
+field.onclick = function (e) {
+  let left = ball.style.left = e.pageX + 'px';
+  let top = ball.style.top = e.pageY + 'px';
+  ballArray.push({
+    left,
+    top
+  });
 }
 
-function goRepeat(){
-     let i = 0;
-      let startTimert = setInterval( function (){
-        ball.style.left = ballArray[i].left;
-        ball.style.top = ballArray[i].top;
-        i++;
-        if(i === ballArray.length){
-          clearInterval(startTimert);
-        }
-      }, 800);
+function goRepeat() {
+  let i = 0;
+  let startTimert = setInterval(function () {
+    /* setInterval is a cycle therefore array iteration through the background is not needed */
+    ball.style.left = ballArray[i].left;
+    ball.style.top = ballArray[i].top;
+    i++;
+    if (i === ballArray.length) {
+      /* stop cycle repeat */
+      clearInterval(startTimert);
     }
+  }, 800);
+}
