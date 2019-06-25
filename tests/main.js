7209d9d60d0bf4c731aef68654edce2568637dab
Alex Zhorov <3767082@mail.ru>
2019-06-25 12:25:09 +0300

    Upload test

---
+++ tests/main.js
@@ -0,0 +1,21 @@
+
+
+function Arr(){
+  this[0] = 1;
+  this[1] = 2;
+  this[2] = 3;
+}
+
+function Obj(){
+  let slice = function(){console.log(321);}
+}
+
+Arr.prototype = Object.create(Obj.prototype);
+
+let newArr = new Arr;
+let newObj = new Obj;
+
+console.dir([1,2,3]);
+
+
+// console.log(newArr);
